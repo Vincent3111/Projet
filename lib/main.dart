19f2c090a75c2c6801f171075207c13467dd9174
Vincent3111 <78927439+Vincent3111@users.noreply.github.com>
2021-02-25 14:13:12 -0500

    modification main.dart
    commentaire

--- lib/main.dart
+++ lib/main.dart
@@ -15,170 +15,180 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Easy QR',
-      color:Colors.black,
+      color: Colors.black,
       debugShowCheckedModeBanner: false,
-      routes: <String, WidgetBuilder> {
+      routes: <String, WidgetBuilder>{
         '/register': (BuildContext context) => new Register(),
         '/generate': (BuildContext context) => new GeneratePage(),
         '/homepage': (BuildContext context) => new HomePage(),
         '/scan': (BuildContext context) => new ScanPage(),
       },
-
-      home:MyHomePage(),
-
+      home: MyHomePage(),
     );
   }
 }
 
+//blablabla
 class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-        backgroundColor:Colors.blue,
-        body: Column(
-        children: <Widget>[
-        Expanded(
-        flex: 4,
-        child:Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(420.0)),
-    color: Colors.white,
-    ),
-          child: Padding(
-            padding: const EdgeInsets.all(32.0),
-            child:Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-            SizedBox(height: 32.0),
-            Container(
-                width: 300.0,
-                child:Text(
-                  'Easy QR',
-                  style: TextStyle(
-                     color: Colors.blue, fontWeight: FontWeight.bold, fontSize:32.0),
-
-                ),
-            ),
-                  Container(
-                    width:356.0,
-                    height:250.0,
-                    child:Image.network("https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX40478458.jpg"),
-
-                  ),
-            Container(
-              child:Text("Computing & Information Systems", style:TextStyle(fontSize: 20.0,color: Colors.blueAccent)),
-            ),SizedBox(height: 15.0),
-                  Container(
-
-                    child:Text("SUSL", style:TextStyle(fontSize: 20.0,color: Colors.blueAccent)),
-                  ),
-            SizedBox(height:60.0),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                FloatingActionButton(
-                  onPressed: (){
-                    Navigator.push(context,MaterialPageRoute(
-                        builder: (context)=> DetailScreen()
-                    ),
-                    );
-                  },
-                  backgroundColor:Colors.blueAccent,
-                  child: Icon(Icons.arrow_right),
-                ),
-            ],
-        ),
-            ],
-        ),
-        ),
-        ),
-        ),
-    ],
-
-    ),
-    );
-  }
-}
-class DetailScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-   backgroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 4,
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(420.0)),
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 32.0),
+                    Container(
+                      width: 300.0,
+                      child: Text(
+                        'Easy QR',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 32.0),
+                      ),
+                    ),
+                    Container(
+                      width: 356.0,
+                      height: 250.0,
+                      child: Image.network(
+                          "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX40478458.jpg"),
+                    ),
+                    Container(
+                      child: Text("Computing & Information Systems",
+                          style: TextStyle(
+                              fontSize: 20.0, color: Colors.blueAccent)),
+                    ),
+                    SizedBox(height: 15.0),
+                    Container(
+                      child: Text("SUSL",
+                          style: TextStyle(
+                              fontSize: 20.0, color: Colors.blueAccent)),
+                    ),
+                    SizedBox(height: 60.0),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        FloatingActionButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => DetailScreen()),
+                            );
+                          },
+                          backgroundColor: Colors.blueAccent,
+                          child: Icon(Icons.arrow_right),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        title: Text('Easy QR',style: TextStyle(fontSize: 17.0),),
+        title: Text(
+          'Easy QR',
+          style: TextStyle(fontSize: 17.0),
+        ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text('WELCOME',
-            style: TextStyle(color: Colors.blueAccent,fontSize: 20.0, fontWeight: FontWeight.bold),),
-       SizedBox(height:20.0),
+          Text(
+            'WELCOME',
+            style: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20.0),
           RaisedButton(
-            child:Text(
-                "ADMIN",
+            child: Text(
+              "ADMIN",
               style: TextStyle(
-              color: Colors.black,
-              letterSpacing: 1.5,
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-
+                color: Colors.black,
+                letterSpacing: 1.5,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            ),
-            onPressed: (){
+            onPressed: () {
               Navigator.of(context).push(
-                MaterialPageRoute(builder: (context)=> Admin()),
+                MaterialPageRoute(builder: (context) => Admin()),
               );
             },
             shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),side: BorderSide(color: Colors.blue,width:3.0)
-            ),
+                borderRadius: BorderRadius.circular(20.0),
+                side: BorderSide(color: Colors.blue, width: 3.0)),
           ),
           SizedBox(height: 10.0),
           RaisedButton(
-            child:Text(
+            child: Text(
               "LECTURER",
               style: TextStyle(
                 color: Colors.black,
                 letterSpacing: 1.5,
                 fontSize: 18.0,
                 fontWeight: FontWeight.bold,
-
               ),
             ),
-            onPressed: (){
+            onPressed: () {
               Navigator.of(context).push(
-                MaterialPageRoute(builder: (context)=> Lecturer()),
+                MaterialPageRoute(builder: (context) => Lecturer()),
               );
             },
             shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),side: BorderSide(color: Colors.blue,width:3.0)
-            ),
+                borderRadius: BorderRadius.circular(20.0),
+                side: BorderSide(color: Colors.blue, width: 3.0)),
           ),
-          SizedBox(height:10.0),
+          SizedBox(height: 10.0),
           RaisedButton(
-            child:Text(
+            child: Text(
               "STUDENT",
               style: TextStyle(
                 color: Colors.black,
                 letterSpacing: 1.5,
                 fontSize: 18.0,
                 fontWeight: FontWeight.bold,
-
               ),
             ),
-            onPressed: (){
+            onPressed: () {
               Navigator.of(context).push(
-                MaterialPageRoute(builder: (context)=> Students()),
+                MaterialPageRoute(builder: (context) => Students()),
               );
             },
             shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),side: BorderSide(color: Colors.blue,width:3.0)
-            ),
+                borderRadius: BorderRadius.circular(20.0),
+                side: BorderSide(color: Colors.blue, width: 3.0)),
           ),
         ],
       ),
